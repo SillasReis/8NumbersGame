56024a329f459f2d4f5e036aee0c540af3c53749
Sillas Reis <sillas.dev@gmail.com>
2020-09-08 12:13:53 -0300

    Movimentando números

--- game.py
+++ game.py
@@ -11,17 +11,21 @@ class Game(Frame):
         self.initUI()
         self.initNumbers()
     
+
     def initUI(self):
         self.master.title('Jogo dos 8 números')
         self.grid()
 
+
     def initNumbers(self):
-        pixel = PhotoImage(width=1, height=1)
+        self.pixel = PhotoImage(width=1, height=1)
 
         for i in range(8):
             number = i+1
-            self.numbers[number] = Button(self, image=pixel, name=str(number), text=str(number), bg='lightgreen', width=200, height=200, compound='c')
-        
+            self.numbers[number] = Button(self, image=self.pixel, name=str(number), text=str(number), bg='lightgreen', width=200, height=200, compound=LEFT)
+            bt = self.numbers[number]
+            bt.configure(command = lambda bt=bt : self.numbersClick(bt))
+
         self.numbers[1].grid(row=0, column=0)
         self.numbers[2].grid(row=0, column=1)
         self.numbers[3].grid(row=0, column=2)
@@ -31,8 +35,36 @@ class Game(Frame):
         self.numbers[7].grid(row=2, column=0)
         self.numbers[8].grid(row=1, column=0)
 
-        self.empty = Button(self, image=pixel, name='empty', text='', bg='green', width=200, height=200, compound='c')
+        self.empty = Frame(self, name='empty', bg='green', width=200, height=200, border=0)
         self.empty.grid(row=1, column=1)
+
+
+    def numbersClick(self, number_button):
+        number_position = number_button.grid_info()
+        empty_position = self.empty.grid_info()
+
+        row_compare = number_position['row'] == empty_position['row']
+        column_compare = number_position['column'] == empty_position['column']
+
+        #Verificar se são vizinhos diretos
+        valid_positions = False
+
+        if row_compare ^ column_compare:
+            if row_compare:
+                if abs(number_position['column'] - empty_position['column']) == 1:
+                    valid_positions = True
+
+            elif column_compare:
+                if abs(number_position['row'] - empty_position['row']) == 1:
+                    valid_positions = True
+
+        #Trocar espaço vazio com número selecionado
+        if valid_positions:
+            number_button.grid_forget()
+            self.empty.grid_forget()
+
+            number_button.grid(row=empty_position['row'], column=empty_position['column'])
+            self.empty.grid(row=number_position['row'], column=number_position['column'])
 
 
 root = Tk()
