d07879b2ed2096d4d4da2662cedc7a7fe506c39f
Sillas Reis <sillas.dev@gmail.com>
2020-09-09 14:09:22 -0300

    Gerando posições aleatórias para os números

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import copy
+from random import randint
 
 
 class GameOverException(Exception):
@@ -18,8 +19,7 @@ class Game():
             7:(2,0),
             8:(1,0)
         }
-        self.current_state = copy.deepcopy(self.finish_state)
-        self.empty = (1,1)
+        self.current_state, self.empty = self.__randomPositions()
         
         self.plays = 0
 
@@ -71,3 +71,30 @@ class Game():
         
         raise GameOverException()
 
+    
+    # Retorna uma tupla:
+        # Posição 0: Dicionário com posições(tuplas) únicas e aleatórias para cada chave. Dicionário é sempre diferente de self.finish_state
+        # Posição 1: Tupla para o espaço vazio
+    def __randomPositions(self):
+        while True:
+            numbers_positions = dict()
+            empty_position = tuple()
+            positions = list()
+            
+            # Gerar tuplas únicas e aleatórias
+            while len(positions) < 9:
+                rand_pos = randint(0,2), randint(0,2)
+                
+                if rand_pos not in positions:
+                    positions.append(rand_pos)
+            
+            # Associar cada tupla a uma chave no dicionário
+            for i in range(1,9):
+                numbers_positions[i] = positions[i-1]
+
+            # Espaço vazio recebe a última tupla
+            empty_position = positions[8]
+
+            # Verificar se dados gerados não são iguais ao self.finish_state
+            if numbers_positions != self.finish_state:
+                return numbers_positions, empty_position
